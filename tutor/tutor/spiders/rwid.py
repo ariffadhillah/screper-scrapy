ffea64abd0cf9b3e821f122c33a341766bf68464
arif fadhillah <ariffadhillah319@gmail.com>
2021-01-04 04:59:24 -0500

    done

--- tutor/tutor/spiders/rwid.py
+++ tutor/tutor/spiders/rwid.py
@@ -1,4 +1,7 @@
+from typing import List
+
 import scrapy
+from cssselect import Selector
 
 
 class RwidSpider(scrapy.Spider):
@@ -20,7 +23,64 @@ class RwidSpider(scrapy.Spider):
             formdata=data,
             callback=self.after_login
         )
+
     # end
     def after_login(self, response):
-        yield {"title": response.css("title::text").get()}
+        """"
+        step ke 2
+        1. ambil semua data data barang yang ada di halaman -> akan menuju detail (parsing detail)
+        2. ambil semua link next. -> akan balik ke self:after_login
+
+        :param response
+        :return
+        """
+
+        # get detail produck 1
+        detail_products: List[Selector] = response.css(".card .card-title a")
+        for detail in detail_products:
+            href = detail.attrib.get("href")
+            yield response.follow(href, callback=self.parse_detail)
+
+        paginations: List[Selector] = response.css(".pagination a.page-link")
+        for pagination in paginations:
+            href = pagination.attrib.get("href")
+            yield response.follow(href, callback=self.after_login)
+
+
+    def parse_detail(self, response):
+        image = response.css(".card-img-top").attrib.get("src")
+
+        title = response.css(".card-title::text").get()
+        stock = response.css(".card-stock::text").get()
+        description = response.css(".card-text::text").get()
+
+        return {
+            "image": image,
+            "title": title,
+            "stock": stock,
+            "desc": description
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
